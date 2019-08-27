91079454d3db8a3314c73171ab76d567de860849
guimiran <mirian@mirian-PC>
2019-08-28 01:38:09 +0200

    add

---
+++ src/main/java/automatizacion/framework/App.java
@@ -0,0 +1,13 @@
+package automatizacion.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
